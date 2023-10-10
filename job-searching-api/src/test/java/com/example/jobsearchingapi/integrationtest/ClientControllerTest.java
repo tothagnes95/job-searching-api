a6977cf3bacf13fe1e90db62f1b2659d29b01178
tothagnes95 <tothagnes95@gmail.com>
2023-10-10 18:58:22 +0200

    test: unit tests are created, integration tests are still failing

---
+++ job-searching-api/src/test/java/com/example/jobsearchingapi/integrationtest/ClientControllerTest.java
@@ -0,0 +1,54 @@
+package com.example.jobsearchingapi.integrationtest;
+
+import com.example.jobsearchingapi.controllers.ClientController;
+import com.example.jobsearchingapi.models.Client;
+import com.example.jobsearchingapi.models.DTOs.ClientDTO;
+import com.example.jobsearchingapi.repositories.ClientRepository;
+import com.example.jobsearchingapi.services.ClientService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class ClientControllerTest {
+  private final ObjectMapper mapper = new ObjectMapper();
+
+  @Autowired
+  private ClientRepository clientRepository;
+  @Mock private ClientService clientService;
+  @InjectMocks
+  private ClientController clientController;
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  public void register_OK_test() throws Exception {
+    ClientDTO clientDTO = new ClientDTO("agi", "agi@job.com");
+    mockMvc.perform(post("/register")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(mapper.writeValueAsString(clientDTO)))
+            .andExpect(status().is(200));
+  }
+
+  @Test
+  public void register_missing_name() throws Exception{
+    ClientDTO clientDTO = new ClientDTO(null, "agi@job.com");
+    mockMvc.perform(post("/register")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(mapper.writeValueAsString(clientDTO)))
+            .andExpect(status().is(400));
+  }
+}
---
+++ job-searching-api/src/test/java/com/example/jobsearchingapi/unittest/ClientServiceTest.java
@@ -0,0 +1,91 @@
+package com.example.jobsearchingapi.unittest;
+
+import com.example.jobsearchingapi.exceptions.InvalidInputException;
+import com.example.jobsearchingapi.exceptions.ResourceNotFoundException;
+import com.example.jobsearchingapi.models.DTOs.ClientDTO;
+import com.example.jobsearchingapi.repositories.ClientRepository;
+import com.example.jobsearchingapi.services.ClientServiceImp;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+public class ClientServiceTest {
+  @Mock private ClientRepository clientRepository;
+  @InjectMocks private ClientServiceImp clientServiceImp;
+
+  @BeforeEach
+  public void setup(){
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  public void isValidEmail_True(){
+    assertTrue(clientServiceImp.isValidEmail("agi@agi.com"));
+  }
+
+  @Test
+  public void isValidEmail_False(){
+    assertTrue(!clientServiceImp.isValidEmail("agi"));
+  }
+
+  @Test
+  public void checkClientDetails_NameAndEmail_Missing(){
+    ClientDTO clientDTO = new ClientDTO(null, null);
+    Exception exception = assertThrows(ResourceNotFoundException.class, () -> clientServiceImp.checkClientDetails(clientDTO));
+    String expectedMessage = "Please provide client name and email";
+
+    assertTrue(expectedMessage.contains(exception.getMessage()));
+  }
+
+  @Test
+  public void checkClientDetails_Name_Missing(){
+    ClientDTO clientDTO = new ClientDTO(null, "agi@agi.com");
+    Exception exception = assertThrows(ResourceNotFoundException.class, () -> clientServiceImp.checkClientDetails(clientDTO));
+    String expectedMessage = "Please provide client name";
+
+    assertTrue(expectedMessage.contains(exception.getMessage()));
+  }
+
+  @Test
+  public void checkClientDetails_Email_Missing(){
+    ClientDTO clientDTO = new ClientDTO("agi", null);
+    Exception exception = assertThrows(ResourceNotFoundException.class, () -> clientServiceImp.checkClientDetails(clientDTO));
+    String expectedMessage = "Please provide client email";
+
+    assertTrue(expectedMessage.contains(exception.getMessage()));
+  }
+
+  @Test
+  public void checkClientDetails_Name_TooLong(){
+    ClientDTO clientDTO = new ClientDTO("LlanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogochLlanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch", "agi@email.com");
+    Exception exception = assertThrows(InvalidInputException.class, () -> clientServiceImp.checkClientDetails(clientDTO));
+    String expectedMessage = "The provided name must be shorter than 100 character";
+
+    assertTrue(expectedMessage.contains(exception.getMessage()));
+  }
+
+  @Test
+  public void checkClientDetails_Email_Invalid(){
+    ClientDTO clientDTO = new ClientDTO("agi", "agi");
+    Exception exception = assertThrows(InvalidInputException.class, () -> clientServiceImp.checkClientDetails(clientDTO));
+    String expectedMessage = "Please provide an email in the following format: input@input.com";
+
+    assertTrue(expectedMessage.contains(exception.getMessage()));
+  }
+
+  @Test
+  public void checkClientDetails_Email_IsTaken(){
+    ClientDTO clientDTO = new ClientDTO("agi", "agi@email.com");
+    when(clientRepository.existsByEmail(clientDTO.getEmail())).thenReturn(true);
+    Exception exception = assertThrows(InvalidInputException.class, () -> clientServiceImp.checkClientDetails(clientDTO));
+    String expectedMessage = "The provided email is already taken, please choose another one";
+
+    assertTrue(expectedMessage.contains(exception.getMessage()));
+  }
+}
